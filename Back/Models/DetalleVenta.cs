7b93a33e8609f739c92ef594ca88a7ab25e03c99
Sandre06 <utm24090875@utma.edu.mx>
2025-11-10 21:08:49 -0600

    models

--- Back/Models/DetalleVenta.cs
+++ Back/Models/DetalleVenta.cs
@@ -5,10 +5,10 @@
         public int IdDetalle { get; set; }
         public int IdVenta { get; set; }
         public Ventas Venta { get; set; }
+        public int IdProducto { get; set; }
         public Productos Producto {get; set; }
         public string Cantidad { get; set; }
         public double Subtotal { get; set; }
-        public ICollection<Productos> IdProducto { get; set; }
 
     }
 }
--- Back/Models/Productos.cs
+++ Back/Models/Productos.cs
@@ -3,13 +3,13 @@
     public class Productos
     {
         public int IdProducto { get; set; }
+        public int IdCategoria { get; set; }
         public Categorias Categoria {  get; set; }
+        public int IdProveedor { get; set; }
         public Proveedores Proveedor { get; set; }
         public string Nombre { get; set; }
         public string Precio { get; set; }
         public string Stock { get; set; }
-        public ICollection<Categorias> IdCategoria { get; set; }
-        public ICollection<Proveedores> IdProveedor { get; set; }
         public ICollection<Inventario> IdInventario { get; set; }
         public ICollection<DetalleVenta> IdDetalleVenta { get; set; }
     }
